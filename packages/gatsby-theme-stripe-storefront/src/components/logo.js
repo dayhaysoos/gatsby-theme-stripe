a25ab4837f66b9b70edc3897cc34a687e9b3743a
dayhaysoos <ndejesus1227@gmail.com>
2019-10-20 23:21:24 -0400

    slight a11y adjustments

--- packages/gatsby-theme-stripe-storefront/src/components/logo.js
+++ packages/gatsby-theme-stripe-storefront/src/components/logo.js
@@ -5,7 +5,11 @@ import { jsx } from 'theme-ui'
 const Logo = ({ logo }) => {
   return (
     <>
-      <img css={{ height: '75px', width: '75px' }} src={logo} />
+      <img
+        alt={'company logo'}
+        css={{ height: '75px', width: '75px' }}
+        src={logo}
+      />
     </>
   )
 }
