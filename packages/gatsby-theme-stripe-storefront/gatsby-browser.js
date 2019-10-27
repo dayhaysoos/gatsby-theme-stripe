1c69b9113858389c471e41427ed137ccc8525bb4
dayhaysoos <ndejesus1227@gmail.com>
2019-10-27 19:19:46 -0400

    fixed index page layout

--- packages/gatsby-theme-stripe-storefront/gatsby-browser.js
+++ packages/gatsby-theme-stripe-storefront/gatsby-browser.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const { CartProvider } = require('./src/context/shopping-cart')
 
-exports.wrapRootElement = ({ element }, { stripePublicKey }) => {
+export const wrapRootElement = ({ element }, { stripePublicKey }) => {
   const isBrowser = typeof window !== 'undefined'
 
   let stripe = isBrowser ? window.Stripe(stripePublicKey) : null
--- packages/gatsby-theme-stripe-storefront/src/pages/index.js
+++ packages/gatsby-theme-stripe-storefront/src/pages/index.js
@@ -1,21 +1,16 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx } from 'theme-ui'
-import Layout from '../components/layout'
 import SkuList from '../components/sku-list'
 import SEO from '../components/SEO'
-import Copy from '../components/copy'
-
-import strings from '../strings.json'
 
 const App = () => {
   return (
-    <Layout>
-      <Copy copy={strings.storefront} />
+    <>
       <SEO>
         <SkuList />
       </SEO>
-    </Layout>
+    </>
   )
 }
 
