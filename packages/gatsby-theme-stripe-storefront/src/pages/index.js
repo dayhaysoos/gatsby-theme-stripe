8858434f8c640afd231f4de8663afc04d096bc26
dayhaysoos <ndejesus1227@gmail.com>
2019-10-27 19:48:50 -0400

    added right pane to initial layout

--- packages/gatsby-theme-stripe-storefront/src/pages/index.js
+++ packages/gatsby-theme-stripe-storefront/src/pages/index.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { jsx } from 'theme-ui'
 import SkuList from '../components/sku-list'
 import SEO from '../components/SEO'
+import RightPane from '../components/right-pane'
 
 const App = () => {
   return (
     <>
       <SEO>
+        <RightPane />
         <SkuList />
       </SEO>
     </>
