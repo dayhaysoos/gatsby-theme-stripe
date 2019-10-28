7c486080c66af86ab1941c414470b9fe7898d640
dayhaysoos <ndejesus1227@gmail.com>
2019-10-28 10:43:07 -0400

    created demo from site storefront

---
+++ examples/storefront-site/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from 'gatsby-theme-stripe-storefront/src/components/layout'
+import RightPane from 'gatsby-theme-stripe-storefront/src/components/right-pane'
+import SkuList from 'gatsby-theme-stripe-storefront/src/components/sku-list'
+
+export default () => (
+  <Layout>
+    <RightPane />
+    <SkuList />
+  </Layout>
+)
