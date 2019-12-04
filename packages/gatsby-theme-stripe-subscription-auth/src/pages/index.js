99c776b9fc1b0158a45d94667b607df3f1f75c3f
dayhaysoos <ndejesus1227@gmail.com>
2019-12-03 22:48:33 -0500

    make firestore calls and statemanagement

--- packages/gatsby-theme-stripe-subscription-auth/src/pages/index.js
+++ packages/gatsby-theme-stripe-subscription-auth/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   auth,
   useAuth,
@@ -7,19 +7,44 @@ import {
 } from 'gatsby-theme-firebase'
 import { useSubscription } from 'gatsby-theme-stripe-subscription/src/context/subscription'
 import SubscriptionList from 'gatsby-theme-stripe-subscription/src/components/subscription-list'
+import ContentProtector from '../components/content-protector'
+import { navigate } from 'gatsby'
 
 const SubscriptionPage = () => {
   const { isLoggedIn } = useAuth()
   const { allStripePlan } = useSubscription()
+  const [user, setUsers] = useState()
 
-  const [users, isLoading] = useFirestoreQuery(
-    firestore.collection('users').get()
-  )
+  useEffect(() => {
+    if (!user && auth.currentUser) {
+      const userData = firestore
+        .collection('users')
+        .where('email', '==', auth.currentUser.email)
+        .get()
+        .then(snapshot => {
+          snapshot.forEach(doc => setUsers(doc.data()))
+        })
+    }
+  })
+
+
 
   return (
     <section>
-      {isLoggedIn ? 'Welcome' : 'Log in pls'}
+      {isLoggedIn ? `Welcome ${auth.currentUser.displayName}` : 'Log in pls'}
+      {isLoggedIn ? (
+        <button onClick={() => auth.signOut()}>Log out</button>
+      ) : (
+        <button onClick={() => navigate('/login')}>Log in</button>
+      )}
+
       <SubscriptionList allStripePlan={allStripePlan} />
+      <ContentProtector
+        guardMessage={'Please Subscribe to see content'}
+        shouldShow={user && user.plan === 'Gold'}
+      >
+        <h2>Showing some really good content</h2>
+      </ContentProtector>
     </section>
   )
 }
