edc74c51f16a52b217fec9ea727d58ae830c1989
Nick DeJesus <ndejesus1227@gmail.com>
2019-12-22 11:38:51 -0500

    Update header.js

--- packages/gatsby-theme-stripe-storefront/src/components/header.js
+++ packages/gatsby-theme-stripe-storefront/src/components/header.js
@@ -1,13 +1,14 @@
 /** @jsx jsx */
-import React from 'react'
 import { Header as ThemeHeader, jsx } from 'theme-ui'
+import { Link } from 'gatsby';
 import ShoppingCartIcon from './shopping-cart-icon'
 import Logo from './logo'
 import logo from '../images/logo.jpeg'
 
-const Header = () => (
+const Header = ({links}) => (
   <ThemeHeader>
     <Logo logo={logo} />
+    {links.map(link => <Link sx={{color: 'text'}} to={link}>{link.toUpperCase()}</Link>)}
     <ShoppingCartIcon />
   </ThemeHeader>
 )
