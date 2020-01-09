a0b51f69f5b8f75b02b89a5ab6e7cfb016795d5e
dayhaysoos <ndejesus1227@gmail.com>
2020-01-08 21:03:25 -0500

    removing log

--- packages/gatsby-theme-stripe-subscription/src/components/product-card.js
+++ packages/gatsby-theme-stripe-subscription/src/components/product-card.js
@@ -4,7 +4,6 @@ import { Card, CardContent, Typography } from '@material-ui/core'
 import SubscribeButton from './subscribe-button'
 
 const ProductCard = ({ plan }) => {
-  console.log('plan', plan)
   return (
     <Card
       key={plan.planID}
