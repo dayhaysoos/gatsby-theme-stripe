46ed429d2f742e9b9713cc0891a8a2e3d34b8df3
dayhaysoos <ndejesus1227@gmail.com>
2020-01-20 12:14:37 -0500

    stop yelling at me

--- packages/gatsby-theme-stripe-storefront/src/pages/thank-you.js
+++ packages/gatsby-theme-stripe-storefront/src/pages/thank-you.js
@@ -5,10 +5,10 @@ import Layout from '../components/layout'
 const ThankYou = () => {
   const isBrowser = typeof window !== 'undefined'
 
-  isBrowser ? window.localStorage.removeItem('skus') : null
+  isBrowser ? window.localStorage.removeItem('skus') : console.log('No browser')
 
   setTimeout(() => {
-    isBrowser ? (window.location.href = '/') : null
+    isBrowser ? (window.location.href = '/') : console.log('No browser')
   }, 2000)
   return (
     <Layout>
