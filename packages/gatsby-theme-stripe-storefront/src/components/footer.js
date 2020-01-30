f977386eb1d963fd025115136e03b4f42881bb6f
dayhaysoos <ndejesus1227@gmail.com>
2020-01-30 14:26:14 -0500

    added footer

---
+++ packages/gatsby-theme-stripe-storefront/src/components/footer.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+
+const Footer = () => (
+  <footer
+    sx={{
+      backgroundColor: 'primary',
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    <h2 sx={{ color: 'primaryText' }}>Made by @dayhaysoos</h2>
+  </footer>
+)
+
+export default Footer
--- packages/gatsby-theme-stripe-storefront/src/components/layout.js
+++ packages/gatsby-theme-stripe-storefront/src/components/layout.js
@@ -1,15 +1,17 @@
 /** @jsx jsx */
-import React from 'react'
+
 import { Layout as ThemeLayout, Main, jsx } from 'theme-ui'
 import Header from './header'
 import RightPane from './right-pane'
 import logo from '../images/logo.jpeg'
+import Footer from './footer'
 
 const Layout = ({ children }) => (
   <ThemeLayout>
     <Header links={['home', 'products', 'about']} logo={logo} />
     <RightPane />
     <Main>{children}</Main>
+    <Footer />
   </ThemeLayout>
 )
 
