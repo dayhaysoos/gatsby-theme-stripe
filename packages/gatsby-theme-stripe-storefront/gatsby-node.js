11858d6eef809b9bd80f86be04b7ce161f857a25
Nick DeJesus <ndejesus1227@gmail.com>
2020-06-28 21:27:01 -0400

    fix path

--- packages/gatsby-theme-stripe-storefront/gatsby-node.js
+++ packages/gatsby-theme-stripe-storefront/gatsby-node.js
@@ -129,10 +129,8 @@ exports.createPages = async ({ graphql, actions }) => {
   result.data.allProduct.edges.forEach(({ node }) => {
     createPage({
       path: `/products/${node.slug}`,
-      component: path.resolve(`./src/templates/product.js`),
+      component: require.resolve(`./src/templates/product.js`),
       context: {
-        // Data passed to context is available
-        // in page queries as GraphQL variables.
         slug: node.slug,
       },
     })
